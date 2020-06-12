23ed72ff5e53c11a8db1c8aef19a9c7f6df6b0a1
otsolap <59653348+otsolap@users.noreply.github.com>
2020-06-12 15:07:57 +0300

    changed 8888 to 5000 on ports.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 // Request .env details from Team-chat.
 var client_id = process.env.SPOTIFY_CLIENT_ID;
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-var redirect_uri = process.env.SPOTIFY_REDIRECT_URI || 8888;
+var redirect_uri = process.env.SPOTIFY_REDIRECT_URI || 5000;
 
 /**
  * Generates a random string containing numbers and letters
@@ -150,5 +150,5 @@ app.get("/refresh_token", function (req, res) {
   });
 });
 
-console.log("Listening on 8888");
-app.listen(8888);
+console.log("Listening on 5000");
+app.listen(5000);
